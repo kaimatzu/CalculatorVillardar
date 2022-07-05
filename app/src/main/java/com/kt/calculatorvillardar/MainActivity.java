58b6ae4e0e2130dc18e49fe0b7c9f17f85d20218
kaimatzu <karljohnvillardar@gmail.com>
2022-07-05 22:28:14 +0800

    Milestone 2: Add event listener to operations

--- app/src/main/java/com/kt/calculatorvillardar/MainActivity.java
+++ app/src/main/java/com/kt/calculatorvillardar/MainActivity.java
@@ -3,39 +3,41 @@ package com.kt.calculatorvillardar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         digitButtonListenerMethod();
+        operationButtonListenerMethod();
     }
 
     public void digitButtonListenerMethod() {
         View.OnClickListener digitListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText etSpace = (EditText) findViewById(R.id.etSpace);
+                EditText etSpace = findViewById(R.id.etSpace);
                 Button btn = (Button) view;
                 etSpace.setText(etSpace.getText() + "" + btn.getText());
             }
         };
 
-        Button btn0 = (Button) findViewById(R.id.btn0);
-        Button btn1 = (Button) findViewById(R.id.btn1);
-        Button btn2 = (Button) findViewById(R.id.btn2);
-        Button btn3 = (Button) findViewById(R.id.btn3);
-        Button btn4 = (Button) findViewById(R.id.btn4);
-        Button btn5 = (Button) findViewById(R.id.btn5);
-        Button btn6 = (Button) findViewById(R.id.btn6);
-        Button btn7 = (Button) findViewById(R.id.btn7);
-        Button btn8 = (Button) findViewById(R.id.btn8);
-        Button btn9 = (Button) findViewById(R.id.btn9);
+        Button btn0 = findViewById(R.id.btn0);
+        Button btn1 = findViewById(R.id.btn1);
+        Button btn2 = findViewById(R.id.btn2);
+        Button btn3 = findViewById(R.id.btn3);
+        Button btn4 = findViewById(R.id.btn4);
+        Button btn5 = findViewById(R.id.btn5);
+        Button btn6 = findViewById(R.id.btn6);
+        Button btn7 = findViewById(R.id.btn7);
+        Button btn8 = findViewById(R.id.btn8);
+        Button btn9 = findViewById(R.id.btn9);
+
         btn0.setOnClickListener(digitListener);
         btn1.setOnClickListener(digitListener);
         btn2.setOnClickListener(digitListener);
@@ -46,5 +48,25 @@ public class MainActivity extends AppCompatActivity {
         btn7.setOnClickListener(digitListener);
         btn8.setOnClickListener(digitListener);
         btn9.setOnClickListener(digitListener);
+    }
+
+    public void operationButtonListenerMethod() {
+        View.OnClickListener operationListener  = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button btn = (Button) v;
+                Log.d("Ops", btn.getText().toString());
+            }
+        };
+
+        Button btnAdd = findViewById(R.id.btnAdd);
+        Button btnSubtract = findViewById(R.id.btnSubtract);
+        Button btnMultiply = findViewById(R.id.btnMultiply);
+        Button btnDivide = findViewById(R.id.btnDivide);
+
+        btnAdd.setOnClickListener(operationListener);
+        btnSubtract.setOnClickListener(operationListener);
+        btnMultiply.setOnClickListener(operationListener);
+        btnDivide.setOnClickListener(operationListener);
     }
 }
